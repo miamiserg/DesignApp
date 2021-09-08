143921a604108b7b8031391cf9c87a9942bcdd95
Giovanni Noa <giovnoa@gmail.com>
2021-09-08 17:33:49 -0400

    Initial Commit

---
+++ IntroApp/IntroAppApp.swift
@@ -0,0 +1,15 @@
+//
+//  IntroAppApp.swift
+//  IntroApp
+//
+
+import SwiftUI
+
+@main
+struct IntroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
